4f712dbd6b28590d1e33128d8ca4dd1a2d1d2200
Prince <princemehta516@gmail.com>
2023-11-27 23:39:03 +0530

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/Navbar';
+import Card from './components/Card';
+
+function App() {
+  return (
+    <div className="">
+      <Navbar />
+      <Card />
+    </div>
+  );
+}
+
+export default App;
